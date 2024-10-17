7520246381e85fe8e8f928a9c15c429c0ed457d1
Sherman Rofeman <skippyr.developer@icloud.com>
2024-10-17 20:33:28 -0300

    Add macros

--- src/Tmk.cpp
+++ src/Tmk.cpp
@@ -3,4 +3,13 @@
 
 namespace Tmk
 {
+    Terminal::StreamRedirectionCache::StreamRedirectionCache(bool isInputRedirected, bool isOutputRedirected, bool isErrorRedirected)
+        : m_isInputRedirected(isInputRedirected), m_isOutputRedirected(isOutputRedirected), m_isErrorRedirected(isErrorRedirected)
+    {
+    }
+
+    bool Terminal::StreamRedirectionCache::IsRedirected(int fileNo) const
+    {
+        return !fileNo ? m_isInputRedirected : fileNo == 1 ? m_isOutputRedirected : m_isErrorRedirected;
+    }
 }
