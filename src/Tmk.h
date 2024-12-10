c356bad79c20c369f90c2f85e353c9f5099fa3ee
Sherman Rofeman <skippyr.developer@icloud.com>
2024-12-10 17:31:37 -0300

    Add first docs

--- src/Tmk.h
+++ src/Tmk.h
@@ -11,29 +11,89 @@
 
 namespace Tmk
 {
+    /// <summary>
+    /// Contains the terminal layers where colors can be set into.
+    /// </summary>
     enum class Layer
     {
+        /// <summary>
+        /// The foreground layer refers to the one where the graphemes are.
+        /// </summary>
         Foreground = 3,
+        /// <summary>
+        /// The background layer refers to the one behind the graphemes.
+        /// </summary>
         Background
     };
 
+    /// <summary>
+    /// Contains the first 16 colors of the ANSI color palette, which map to the colors of the terminal colorscheme.
+    /// </summary>
     enum class AnsiColor
     {
+        /// <summary>
+        /// The dark variant of the black color.
+        /// </summary>
         DarkBlack,
+        /// <summary>
+        /// The dark variant of the red color.
+        /// </summary>
         DarkRed,
+        /// <summary>
+        /// The dark variant of the green color.
+        /// </summary>
         DarkGreen,
+        /// <summary>
+        /// The dark variant of the yellow color.
+        /// </summary>
         DarkYellow,
+        /// <summary>
+        /// The dark variant of the blue color.
+        /// </summary>
         DarkBlue,
+        /// <summary>
+        /// The dark variant of the magenta color.
+        /// </summary>
         DarkMagenta,
+        /// <summary>
+        /// The dark variant of the cyan color.
+        /// </summary>
         DarkCyan,
+        /// <summary>
+        /// The dark variant of the white color.
+        /// </summary>
         DarkWhite,
+        /// <summary>
+        /// The light variant of the black color.
+        /// </summary>
         LightBlack,
+        /// <summary>
+        /// The light variant of the red color.
+        /// </summary>
         LightRed,
+        /// <summary>
+        /// The light variant of the green color.
+        /// </summary>
         LightGreen,
+        /// <summary>
+        /// The light variant of the yellow color.
+        /// </summary>
         LightYellow,
+        /// <summary>
+        /// The light variant of the blue color.
+        /// </summary>
         LightBlue,
+        /// <summary>
+        /// The light variant of the magenta color.
+        /// </summary>
         LightMagenta,
+        /// <summary>
+        /// The light variant of the cyan color.
+        /// </summary>
         LightCyan,
+        /// <summary>
+        /// The light variant of the white color.
+        /// </summary>
         LightWhite
     };
 
