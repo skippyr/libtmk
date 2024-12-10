51b9f087248b262ca5f20d1bd65d24dd6e2ce463
Sherman Rofeman <skippyr.developer@icloud.com>
2024-12-10 08:43:50 -0300

    Add write functions

--- src/tmk.hh
+++ src/tmk.hh
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <iostream>
+#include <format>
 
 namespace tmk {
 class StreamRedirectionException : public std::exception {};
@@ -19,5 +20,29 @@ public:
   static bool isInputRedirected() noexcept;
   static bool isOutputRedirected() noexcept;
   static bool isErrorRedirected() noexcept;
+
+  template <typename ...Args>
+  static void write(std::string_view format, Args... arguments) {
+    init();
+    std::cout << std::vformat(format, std::make_format_args(arguments...));
+  }
+
+  template <typename ...Args>
+  static void writeLine(std::string_view format, Args... arguments) {
+    write(format, arguments...);
+    std::cout << std::endl;
+  }
+
+  template <typename ...Args>
+  static void writeError(std::string_view format, Args... arguments) {
+    init();
+    std::cerr << std::vformat(format, std::make_format_args(arguments...));
+  }
+
+  template <typename ...Args>
+  static void writeErrorLine(std::string_view format, Args... arguments) {
+    writeError(format, arguments...);
+    std::cerr << std::endl;
+  }
 };
 } // namespace tmk
